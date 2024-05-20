80511f63a8507b0498deab7689eb9380f7979a30
Le Hoang Minh Ha <minhha.lehoang@gmail.com>
2024-05-20 20:25:09 +0700

    update

--- modules/model.py
+++ modules/model.py
@@ -1,7 +1,7 @@
 import torch
 import torch.nn as nn
 import torch.nn.functional as F
-
+from typing import List
 
 class LinguisticModule(nn.Module):
     def __init__(self, input_size, hidden_size=64):
@@ -17,14 +17,24 @@ class LinguisticModule(nn.Module):
         return outputs
 
 class SemanticModule(nn.Module):
-    def __init__(self, essay_encoder, dropout=0.1):
+    def __init__(self, essay_encoder, unfreeze, dropout=0.1):
         super(SemanticModule, self).__init__()
-        # freeze
-        for param in essay_encoder.parameters():
-            param.requires_grad = False
-        # # unfreeze the pooler
-        # for param in essay_encoder.pooler.parameters():
-        #     param.requires_grad = True
+
+        if unfreeze == 'none':
+            for param in essay_encoder.parameters():
+                param.requires_grad = False
+        elif unfreeze == 'all':
+            pass
+        elif unfreeze == 'encoder':
+            for param in essay_encoder.parameters():
+                param.requires_grad = False
+            for param in essay_encoder.encoder.parameters():
+                param.requires_grad = True
+        else: # unfreeze pooler by default
+            for param in essay_encoder.parameters():
+                param.requires_grad = False
+            for param in essay_encoder.pooler.parameters():
+                param.requires_grad = True
 
         self.essay_encoder = essay_encoder
         self.dropout = torch.nn.Dropout(dropout)
@@ -40,12 +50,24 @@ class SemanticModule(nn.Module):
         return pooled_outputs
     
 class CoherenceModule(nn.Module):
-    def __init__(self, sentence_encoder, dropout=0.1):
+    def __init__(self, sentence_encoder, unfreeze, dropout=0.1):
         super(CoherenceModule, self).__init__()
-        for param in sentence_encoder.parameters():
-            param.requires_grad = False
-        for param in sentence_encoder.pooler.parameters():
-            param.requires_grad = True
+        
+        if unfreeze == 'none':
+            for param in sentence_encoder.parameters():
+                param.requires_grad = False
+        elif unfreeze == 'all':
+            pass
+        elif unfreeze == 'encoder':
+            for param in sentence_encoder.parameters():
+                param.requires_grad = False
+            for param in sentence_encoder.encoder.parameters():
+                param.requires_grad = True
+        else: # unfreeze pooler by default
+            for param in sentence_encoder.parameters():
+                param.requires_grad = False
+            for param in sentence_encoder.pooler.parameters():
+                param.requires_grad = True
         self.sentence_encoder = sentence_encoder
 
         self.dropout = nn.Dropout(dropout)
@@ -62,19 +84,33 @@ class CoherenceModule(nn.Module):
 
 
 class LSCModel(nn.Module):
-    def __init__(self, essay_encoder, sentence_encoder,
-                 input_lf_size, hidden_lf_size=64, dropout=0.1):
+    def __init__(self, essay_encoder, essay_unfreeze: str | None,
+                 sentence_encoder, sentence_unfreeze: str | None,
+                 input_lf_size, hidden_lf_size = 64,  hidden_list: List | None = 0, 
+                 dropout=0.1):
         super(LSCModel, self).__init__()
         self.linguistic_module = LinguisticModule(
             input_lf_size, hidden_lf_size)
-        self.semantic_module = SemanticModule(essay_encoder, dropout=dropout)
-        self.coherence_module = CoherenceModule(
-            sentence_encoder, dropout=dropout)
+        self.semantic_module = SemanticModule(essay_encoder, essay_unfreeze, dropout=dropout)
+        self.coherence_module = CoherenceModule(sentence_encoder, sentence_unfreeze, dropout=dropout)
         
-        self.fc = nn.Linear(
-            hidden_lf_size + essay_encoder.config.hidden_size + sentence_encoder.config.hidden_size, 768)
+        # self.fc = nn.Linear(
+        #     hidden_lf_size + essay_encoder.config.hidden_size + sentence_encoder.config.hidden_size, 768)
 
-        self.regressor = nn.Linear(768, 1)
+        # self.regressor = nn.Linear(768, 1)
+
+        last_hidden_size = hidden_lf_size + essay_encoder.config.hidden_size + sentence_encoder.config.hidden_size
+
+        self.hidden_list = hidden_list
+
+        for i, hidden_size in enumerate(hidden_list):
+            setattr(self, f'fc{i}', nn.Linear(last_hidden_size, hidden_size))
+            setattr(self, f'fc{i}_relu', nn.LeakyReLU())
+            setattr(self, f'fc{i}_dropout', nn.Dropout(dropout))
+
+            last_hidden_size = hidden_size
+
+        self.regressor = nn.Linear(last_hidden_size, 1)
 
         self.dropout = nn.Dropout(dropout)
 
@@ -92,9 +128,10 @@ class LSCModel(nn.Module):
 
         outputs = torch.cat([ling_outputs, sem_outputs, coh_infor], dim=-1)
 
-        outputs = self.fc(outputs)
-        outputs = F.leaky_relu(outputs)
-        outputs = self.dropout(outputs)
+        for i in range(len(self.hidden_list)):
+            outputs = getattr(self, f'fc{i}')(outputs)
+            outputs = getattr(self, f'fc{i}_relu')(outputs)
+            outputs = getattr(self, f'fc{i}_dropout')(outputs)
 
         outputs = self.regressor(outputs)
 
